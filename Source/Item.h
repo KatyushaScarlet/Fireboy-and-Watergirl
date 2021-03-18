82908bb577d0b0d21dc1d0060e6124ea2f213d5d
KatyushaScarlet <KatyushaScarlet@Outlook.com>
2021-03-18 22:18:41 +0800

    update

--- Source/Item.h
+++ Source/Item.h
@@ -13,7 +13,7 @@ namespace game_framework {
 		int  GetY1();			//左上角 y 坐标
 		int  GetX2();			//右下角 x 坐标
 		int  GetY2();			//右下角 y 坐标
-		bool GetAccessibility();//获取同行性
+		bool GetAccessibility();//获取通行性
 	protected:
 		int x1, y1, x2, y2;//左上角坐标
 		bool is_accessible();//是否可通过
