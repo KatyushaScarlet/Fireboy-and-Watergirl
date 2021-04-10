b7796ce871117f17180c019bb9c467104c6acf98
KatyushaScarlet <KatyushaScarlet@Outlook.com>
2021-04-11 00:21:47 +0800

    update

--- Source/Platform.h
+++ Source/Platform.h
@@ -6,6 +6,8 @@ namespace game_framework {
 
 	class Platform
 	{
-
+		//两种平台：水平（horizon）、垂直（vertical）
+		//初始位置：x、y
+		//激活位置：x、y
 	};
 }
--- Source/Switch.h
+++ Source/Switch.h
@@ -6,6 +6,7 @@ namespace game_framework {
 
 	class Switch
 	{
-
+		//两种开关：按钮（非持续作用）、拉杆（持续作用）
+		//绑定平台
 	};
 }
