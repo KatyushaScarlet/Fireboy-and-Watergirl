6e839e59cf51f5de03c77e38aa8d6c8f2ade7549
Amumeris <vesper.port@gmail.com>
2021-05-07 11:31:48 +0800

    UPDATE WIND_EFFECT

--- Source/Wind.cpp
+++ Source/Wind.cpp
@@ -25,23 +25,36 @@ namespace game_framework
 		bitmap.AddBitmap("RES\\wind\\1.bmp", RGB(255, 255, 255));
 		bitmap.AddBitmap("RES\\wind\\2.bmp", RGB(255, 255, 255));
 		bitmap.AddBitmap("RES\\wind\\3.bmp", RGB(255, 255, 255));
+		string path = "RES\\wind_effect\\wind_effect00";
+		string extension = ".bmp";
+		for (int i = 0; i <= 29; i++) {
+			string bitmapPath = path + to_string(i) + extension;
+			char* p = (char*)bitmapPath.c_str();
+			bitmap_wind.AddBitmap(p, RGB(0, 0, 0));
+		}
 	}
 
 	void Wind::OnShow()
 	{
-		bitmap_wall.SetTopLeft(x, y);//ÏÈ»­Ç½
+		bitmap_wall.SetTopLeft(x, y);//¥ýµeÀð
 		bitmap_wall.ShowBitmap();
-		bitmap_wall.SetTopLeft(x + 32, y);//ÏÈ»­Ç½
+		bitmap_wall.SetTopLeft(x + 32, y);//¥ýµeÀð
 		bitmap_wall.ShowBitmap();
-		bitmap_wall.SetTopLeft(x + 64, y);//ÏÈ»­Ç½
+		bitmap_wall.SetTopLeft(x + 64, y);//¥ýµeÀð
 		bitmap_wall.ShowBitmap();
 		bitmap.SetTopLeft(x, y);
 		bitmap.OnShow();
 		bitmap.SetDelayCount(1);
+		bitmap_wind.SetDelayCount(1);
+		bitmap_wind.SetTopLeft(x, y-192);
+		bitmap_wind.OnShow();
 	}
 
 	void Wind::OnMove()
 	{
 		bitmap.OnMove();
+		bitmap_wind.OnMove();
 	}
+
+
 }
